b341a8ef69ad4faeb01749f5dedf1191186d79e8
Luana <luanateixeira148@gmail.com>
2021-08-17 04:11:23 +0000

    complete exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,6 +5,9 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+@store1.name = 'Burnaby Heights'
 
 
 # check answer by running: bundle exec ruby exercises/exercise_2.rb
