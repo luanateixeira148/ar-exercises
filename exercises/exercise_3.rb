ed6b9651096111fbd445b88fe7d284ae5d3f6471
Luana <luanateixeira148@gmail.com>
2021-08-17 04:14:41 +0000

    complete exercise 3

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,8 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+@store3 = Store.find_by(id: 3)
+@store3.destroy
+puts Store.count
+
+# check answer by running: bundle exec ruby exercises/exercise_3.rb
